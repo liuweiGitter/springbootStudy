764ddedd0c20439918ea3c3df17cd9da0886a14a
liuwei <liuweixml@163.com>
2019-09-11 09:44:12 +0800

    post

--- 日志+异常+安全/过滤器/请求鉴权/AuthFilter.java
+++ 日志+异常+安全/过滤器/请求鉴权/AuthFilter.java
@@ -3,7 +3,6 @@ package com.telecom.js.noc.hxtnms.operationplan.filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.PostConstruct;
@@ -24,23 +23,21 @@ import java.util.concurrent.TimeUnit;
  */
 //@Order(1)
 //@WebFilter(filterName = "authFilter", urlPatterns = {"/*"})
-//@WebFilter可以注册过滤器，但过滤器将先于Bean被Spring实例化，因此无法注入其它Bean
+//@WebFilter可以注册过滤器，但该注解属于Servlet，而非Spring，不会被Spring扫描
 //可以在配置类中注册过滤器，解决Bean注入问题，详参FilterRegistrationConfig
 public class AuthFilter implements Filter {
+
     @Autowired
     private Environment environment;
     @Autowired
     private RedisTemplate redisTemplateAuth;
 
-    private ValueOperations<String, String> redisValueOper;
-
-    private final String AUTH_FAILED_DISPATCH = "/authFailed";
+    private static final String AUTH_FAILED_DISPATCH = "/authFailed";
     //默认超时10min
     private Long expireTime = 600L;
 
     @PostConstruct
     private void initParam(){
-        redisValueOper = redisTemplateAuth.opsForValue();
         String expireTimeLogin = environment.getProperty("expireTimeLogin");
         if (!StringUtils.isEmpty(expireTimeLogin)){
             expireTime = Long.valueOf(expireTimeLogin);
@@ -49,13 +46,21 @@ public class AuthFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) {
-
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        //从请求的header中获取登录凭证token
-        String token = ((HttpServletRequest) servletRequest).getHeader("token");
+        /***
+         * 从请求的header中或url中获取登录凭证token
+         * post请求取header
+         * get请求优先从header中取，如果取不到，再从url中取
+         */
+        HttpServletRequest request = ((HttpServletRequest) servletRequest);
+        String method = request.getMethod();
+        String token = request.getHeader("token");
+        if ("GET".equals(method) && StringUtils.isEmpty(token)){
+            token = request.getParameter("token");
+        }
         //不存在的token
         if (StringUtils.isEmpty(token)) {
             servletRequest.getRequestDispatcher(AUTH_FAILED_DISPATCH+"/null").forward(servletRequest, servletResponse);
